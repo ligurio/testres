3adf74c3c49281960804246721057a89c81da65c
Sergey Bronnikov <estetus@gmail.com>
2019-08-01 14:48:36 +0300

    WIP subunit v2

--- libtestoutput/parse_subunit_v2.c
+++ libtestoutput/parse_subunit_v2.c
@@ -38,6 +38,7 @@
 
 // https://github.com/testing-cabal/subunit/blob/master/python/subunit/v2.py#L412
 // https://github.com/testing-cabal/subunit
+// https://gist.github.com/ligurio/c173e9af1b766cf6626f7037cbb78d72
 
 #define HI(x)  ((x) >> 8)
 #define LO(x)  ((x) & 0xFF)
@@ -63,6 +64,25 @@ int is_subunit_v2(char* path)
 	} else {
 		return 1;
 	}
+}
+
+const void* read_uint8(const void* buffer, uint8_t* value)
+{
+    const uint8_t* vptr = (uint8_t*)buffer;
+    *value = *vptr;
+    vptr++;
+
+    return vptr;
+}
+
+const void* read_uint16(const void* buffer, uint16_t* value)
+{
+    /* FIXME: network byte order */
+    const uint16_t* vptr = (uint16_t*)buffer;
+    *value = *vptr;
+    vptr++;
+
+    return vptr;
 }
 
 uint32_t read_field(FILE * stream)
@@ -265,5 +285,51 @@ https://github.com/mlabbe/c_date_parse
 UTF-8
 https://github.com/benkasminbullock/unicode-c/blob/master/unicode.c
 https://github.com/clibs/cutef8
+
+*/
+
+/* Simple public domain implementation of the standard CRC32 checksum.
+ * Outputs the checksum for each file given as a command line argument.
+ * Invalid file names and files that cause errors are silently skipped.
+ * The program reads from stdin if it is called with no arguments. */
+
+/* http://home.thep.lu.se/~bjorn/crc/ */
+
+/*
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+uint32_t crc32_for_byte(uint32_t r) {
+  for(int j = 0; j < 8; ++j)
+    r = (r & 1? 0: (uint32_t)0xEDB88320L) ^ r >> 1;
+  return r ^ (uint32_t)0xFF000000L;
+}
+
+void crc32(const void *data, size_t n_bytes, uint32_t* crc) {
+  static uint32_t table[0x100];
+  if(!*table)
+    for(size_t i = 0; i < 0x100; ++i)
+      table[i] = crc32_for_byte(i);
+  for(size_t i = 0; i < n_bytes; ++i)
+    *crc = table[(uint8_t)*crc ^ ((uint8_t*)data)[i]] ^ *crc >> 8;
+}
+
+int main(int ac, char** av) {
+  FILE *fp;
+  char buf[1L << 15];
+  for(int i = ac > 1; i < ac; ++i)
+    if((fp = i? fopen(av[i], "rb"): stdin)) { 
+      uint32_t crc = 0;
+      while(!feof(fp) && !ferror(fp))
+	crc32(buf, fread(buf, 1, sizeof(buf), fp), &crc);
+      if(!ferror(fp))
+	printf("%08x%s%s\n", crc, ac > 2? "\t": "", ac > 2? av[i]: "");
+      if(i)
+	fclose(fp);
+    }
+  return 0;
+}
 
 */
